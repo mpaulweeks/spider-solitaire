cd66b666e68c25333b601688ada857487d230b3f
mpaulweeks <mpaulweeks@gmail.com>
2021-06-18 16:05:44 -0400

    lint

--- src/fe/App.tsx
+++ src/fe/App.tsx
@@ -14,8 +14,8 @@ export function App() {
     return onTrigger;
   }, [boardState, setBoardState]);
 
-  const reset = useCallback((board: Board) => {
-    setBoardState(board.serialize());
+  const reset = useCallback((newBoard: Board) => {
+    setBoardState(newBoard.serialize());
   }, [setBoardState]);
 
   const board = Board.deserialize(boardState);
